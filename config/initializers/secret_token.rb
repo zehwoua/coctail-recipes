305c08226d5528911453ca0602710a5600507e04
zehwoua <zehwoua@hotmail.com>
2012-11-13 14:45:59 +0900

    ruby installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipes::Application.config.secret_token = '4e4c24b7beef8017d9d1722983bd5d44d778644798e10301a61d7eea188f433ca3fa41a2e778372f45d99034f5969890ca1e1a7dada82e2ad53b846d3cd462f6'
